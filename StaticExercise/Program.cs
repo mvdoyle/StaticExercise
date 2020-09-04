ad04ff984d1ba6565f7a492704b7751a651c4625
Michae Doyle <mdoyle@truecoders.io>
2020-09-04 10:47:56 -0500

    done

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celcius = TempConverter.FahrenheitToCelsius(68); // 20
+            var fahrenheit = TempConverter.CelciusToFahrenheit(20); //68
+
+            Console.WriteLine("After conversion:");
+            Console.WriteLine($"celcius {celcius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            var result = (fahrenheit - 32) / 1.8;
+
+            return result;
+        }
+
+        public static double CelciusToFahrenheit(double celsius)
+        {
+            return (celsius * 9) / 5 + 32;
+        }
+    }
+}
